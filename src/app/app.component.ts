1d7792b35e65ff6716033f19f81c7af9c59dfda3
Shawn <shawn@miniasp.com>
2018-11-09 16:20:07 +0800

    oidc~

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
+import { filter, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-auth-oidc-client-test';
+  userData;
+
+  userInfo;
+
+  token = '';
+
+  tokenExpire = true;
+
+  constructor(public oidcSecurityService: OidcSecurityService) {
+    this.oidcSecurityService.getIsModuleSetup().pipe(
+      filter((isModuleSetup: boolean) => isModuleSetup),
+      take(1)
+    ).subscribe((isModuleSetup: boolean) => {
+      this.doCallbackLogicIfRequired();
+    });
+  }
+
+  login() {
+    // if you need to add extra parameters to the login
+    // let culture = 'de-CH';
+    // this.oidcSecurityService.setCustomRequestParameters({ 'ui_locales': culture });
+    this.oidcSecurityService.authorize();
+  }
+
+  logout() {
+    this.oidcSecurityService.logoff();
+  }
+
+  getUserInfo() {
+    this.oidcSecurityService.getUserData().subscribe(
+      userData => {
+        this.userData = userData;
+      });
+
+    this.oidcSecurityService.getUserinfo().subscribe(
+      userInfo => {
+        this.userInfo = userInfo;
+      });
+  }
+
+  getToken() {
+    this.token = this.oidcSecurityService.getToken();
+  }
+
+  getTokenExpire() {
+    this.oidcSecurityService.getIsAuthorized().subscribe(value => {
+      const isAuthorized = value;
+      this.tokenExpire = isAuthorized;
+    });
+  }
+
+  private doCallbackLogicIfRequired() {
+    if (window.location.hash) {
+      this.oidcSecurityService.authorizedCallback();
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import {
+  AuthModule,
+  OidcConfigService,
+  OidcSecurityService,
+  AuthWellKnownEndpoints,
+  OpenIDImplicitFlowConfiguration
+} from 'angular-auth-oidc-client';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -10,9 +19,63 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    AuthModule.forRoot()
   ],
-  providers: [],
+  providers: [OidcConfigService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private oidcSecurityService: OidcSecurityService) {
+    const openIDImplicitFlowConfiguration = new OpenIDImplicitFlowConfiguration();
+    const stsServer = 'https://localhost:5001';
+    const redirectUri = 'http://localhost:4200/landing';
+    const clientId = 'ng-basicsite';
+    const responseType = 'id_token token';
+    const scope = 'openid UserDataApi userInfo';
+
+
+    openIDImplicitFlowConfiguration.stsServer = stsServer;
+    openIDImplicitFlowConfiguration.redirect_url = redirectUri;
+    // The Client MUST validate that the aud (audience) Claim contains its client_id value registered
+    // at the Issuer identified by the iss (issuer) Claim as an audience.
+    // The ID Token MUST be rejected if the ID Token does not list the Client as a valid audience,
+    // or if it contains additional audiences not trusted by the Client.
+    openIDImplicitFlowConfiguration.client_id = clientId;
+    openIDImplicitFlowConfiguration.response_type = responseType;
+    openIDImplicitFlowConfiguration.scope = scope;
+    openIDImplicitFlowConfiguration.post_logout_redirect_uri = `${stsServer}/Unauthorized`;
+    openIDImplicitFlowConfiguration.start_checksession = false;
+
+    openIDImplicitFlowConfiguration.silent_renew = true;
+    openIDImplicitFlowConfiguration.silent_renew_url = `${stsServer}/silent-renew.html`;
+    openIDImplicitFlowConfiguration.post_login_route = '/dataeventrecords';
+    // HTTP 403
+    openIDImplicitFlowConfiguration.forbidden_route = '/Forbidden';
+    // HTTP 401
+    openIDImplicitFlowConfiguration.unauthorized_route = '/Unauthorized';
+    openIDImplicitFlowConfiguration.log_console_warning_active = true;
+    openIDImplicitFlowConfiguration.log_console_debug_active = true;
+    // id_token C8: The iat Claim can be used to reject tokens that were issued too far away from the current time,
+    // limiting the amount of time that nonces need to be stored to prevent attacks.The acceptable range is Client specific.
+    openIDImplicitFlowConfiguration.max_id_token_iat_offset_allowed_in_seconds = 10;
+
+    const authWellKnownEndpoints = new AuthWellKnownEndpoints();
+    authWellKnownEndpoints.issuer = `${stsServer}`;
+
+    authWellKnownEndpoints.jwks_uri = `${stsServer}/.well-known/openid-configuration/jwks`;
+    authWellKnownEndpoints.authorization_endpoint = `${stsServer}/connect/authorize`;
+    authWellKnownEndpoints.token_endpoint = `${stsServer}/connect/token`;
+    authWellKnownEndpoints.userinfo_endpoint = `${stsServer}/connect/userinfo`;
+    authWellKnownEndpoints.end_session_endpoint = `${stsServer}/connect/endsession`;
+    authWellKnownEndpoints.check_session_iframe = `${stsServer}/connect/checksession`;
+    authWellKnownEndpoints.revocation_endpoint = `${stsServer}/connect/revocation`;
+    authWellKnownEndpoints.introspection_endpoint = `${stsServer}/connect/introspect`;
+
+    this.oidcSecurityService.setupModule(
+      openIDImplicitFlowConfiguration,
+      authWellKnownEndpoints
+    );
+  }
+}
